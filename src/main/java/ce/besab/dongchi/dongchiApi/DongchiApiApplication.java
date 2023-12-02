e94ad7d3794de2160a50f4df641241b613869f30
Mohammad-Safari <msafari8051@gmail.com>
2023-12-02 19:36:45 +0330

    [INIT]bootstraping project

---
+++ src/main/java/ce/besab/dongchi/dongchiApi/DongchiApiApplication.java
@@ -0,0 +1,13 @@
+package ce.besab.dongchi.dongchiApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongchiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongchiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ce/besab/dongchi/dongchiApi/DongchiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ce.besab.dongchi.dongchiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongchiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
