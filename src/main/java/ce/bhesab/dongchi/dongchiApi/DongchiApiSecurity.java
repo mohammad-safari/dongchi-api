662a9011594ec7958da1911d445d44d0dc8533ba
Mohammad-Safari <msafari8051@gmail.com>
2023-12-08 10:16:54 +0330

    [ADD]authuentication based on user model info

--- src/main/java/ce/bhesab/dongchi/dongchiApi/DongchiApiSecurity.java
+++ src/main/java/ce/bhesab/dongchi/dongchiApi/DongchiApiSecurity.java
@@ -2,31 +2,31 @@ package ce.bhesab.dongchi.dongchiApi;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Service;
+
+import ce.bhesab.dongchi.dongchiApi.service.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 public class DongchiApiSecurity {
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.builder()
-                .username("username")
-                .password("password")
-                .passwordEncoder(passwordEncoder::encode)
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -45,5 +45,32 @@ public class DongchiApiSecurity {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+            BCryptPasswordEncoder bCryptPasswordEncoder,
+            DongchiUserDetailsService userDetailsService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+}
+
+@Service
+@RequiredArgsConstructor
+class DongchiUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(username));
+        var gauth = new SimpleGrantedAuthority("USER");
+        return new User(username, user.getPassword(), List.of(gauth));
     }
 }
