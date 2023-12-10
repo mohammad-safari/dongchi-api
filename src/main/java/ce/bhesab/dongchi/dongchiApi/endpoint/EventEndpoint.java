19b496afe85352bf25b47d3f97075932bd7772be
Mohammad-Safari <msafari8051@gmail.com>
2023-12-11 00:25:22 +0330

    [ADD]swagger ui

--- src/main/java/ce/bhesab/dongchi/dongchiApi/endpoint/EventEndpoint.java
+++ src/main/java/ce/bhesab/dongchi/dongchiApi/endpoint/EventEndpoint.java
@@ -3,15 +3,17 @@ package ce.bhesab.dongchi.dongchiApi.endpoint;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("event")
+@RestController
+@RequestMapping("event")
 public class EventEndpoint {
-    @GetMapping("")
+    @GetMapping
     public void getEvents(Authentication authentication) {
     }
 
-    @PostMapping("")
+    @PostMapping
     public void addEvents(Authentication authentication) {
     }
 }
